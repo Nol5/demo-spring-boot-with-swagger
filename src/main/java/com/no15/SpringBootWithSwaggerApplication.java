69c5d46f3ad75f1c81b429dfd5ff031254fea9bd
No.15 <lertrungmongkol@gmail.com>
2017-11-12 16:55:30 +0700

    Initial demo-spring-boot-with-swagger

---
+++ src/main/java/com/no15/SpringBootWithSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.no15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSwaggerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/no15/inquiry/InqResponse.java
@@ -0,0 +1,24 @@
+package com.no15.inquiry;
+
+public class InqResponse {
+
+    public String name;
+    public String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
